f430bfc59c5c724f4e0c3ae3be58539d4adec75e
Diana Moretti <morettidn@gmail.com>
2024-04-08 13:48:37 +0200

    adding update and delete contact functionalities

--- routes/contacts__routes.js
+++ routes/contacts__routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongodb = require("mongodb");
 
 const db = require("../data/database");
+
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -20,13 +23,56 @@ router.post("/", async function (req, res) {
     phone: req.body.phone,
   };
 
-  const result = await db.getDb().collection("contacts").insertOne(newContact);
-  console.log(result);
+  const data = await db.getDb().collection("contacts").insertOne(newContact);
+  console.log(data);
   res.redirect("/");
 });
 
-router.get("/update-contact", function (req, res) {
-  res.render("update__contact");
+router.get("/:id/edit", async function (req, res) {
+  const contactId = req.params.id;
+
+  const contact = await db
+    .getDb()
+    .collection("contacts")
+    .findOne({ _id: new ObjectId(contactId) });
+
+  if (!contact) {
+    return res.status(404).render("404");
+  }
+
+  res.render("update__contact", {
+    contact: contact,
+  });
+});
+
+router.post("/:id/edit", async function (req, res) {
+  const contactId = new ObjectId(req.params.id);
+  const data = await db
+    .getDb()
+    .collection("contacts")
+    .updateOne(
+      { _id: contactId },
+      {
+        $set: {
+          name: req.body.name,
+          email: req.body.email,
+          phone: req.body.phone,
+        },
+      }
+    );
+  console.log(data);
+  res.redirect("/");
+});
+
+router.post("/:id/delete", async function (req, res) {
+  const contactId = new ObjectId(req.params.id);
+  const data = await db
+    .getDb()
+    .collection("contacts")
+    .deleteOne({ _id: contactId });
+
+  console.log(data);
+  res.redirect("/");
 });
 
 module.exports = router;
